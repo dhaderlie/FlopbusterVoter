65e0a1611241f550d7b44db8c1b1ed4ab1f4cfd9
Donaven Haderlie <Donaven.Haderlie@inl.gov>
2022-04-11 12:48:21 -0600

    Initial application build and push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'flopbusterVoting';
+  flops = {
+    a: "Waterworld - 1995",
+    b: "TMNT: Out of the Shadows - 2016",
+    c: "Dudley Do-Right - 1999",
+    d: "TommorrowLand - 2015"
+  }
+  selectedTitle: string = "";
+  SelectedIndex: number = -1
+  
+  voteClicked(event: Event, index:number) {
+    // console.log('voteClicked', (event.target as HTMLInputElement).value);
+    this.selectedTitle = (<HTMLInputElement>event.target).value;
+
+    if (this.isSelected(index)) {
+      this.SelectedIndex = -1;
+      this.selectedTitle = "";
+    } else {
+      this.SelectedIndex = index;
+    }
+  }
+
+  getButtonText(index:number) {
+    if (this.isSelected(index)) {
+      return "Unvote";
+    } else {
+      return "Vote";
+    }
+  }
+
+  anySelected() {
+    return this.SelectedIndex != -1;
+  }
+
+  isSelected(index:number) {
+    return this.SelectedIndex === index;
+  }
+  
 }
