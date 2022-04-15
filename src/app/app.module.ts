0170f97c453a0e6385e26e8e67464b85c1697be7
Donaven Haderlie <Donaven.Haderlie@inl.gov>
2022-04-15 12:26:28 -0600

    adding files to build a rental cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDisplayComponent } from './movie-display/movie-display.component';
+import { FlopVotesComponent } from './flop-votes/flop-votes.component';
+import { RentalCartComponent } from './rental-cart/rental-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDisplayComponent,
+    FlopVotesComponent,
+    RentalCartComponent
   ],
   imports: [
     BrowserModule,
